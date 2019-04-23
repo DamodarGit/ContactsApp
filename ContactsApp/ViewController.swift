1383c50d1cf3022fc765098f0364628ea52b66ab
NDAMODA <harman.damodar@daimler.com>
2019-04-23 12:41:57 +0530

    setup

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Damodar, Namala (623-Extern) on 23/04/19.
+//  Copyright © 2019 Damodar, Namala (623-Extern). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
